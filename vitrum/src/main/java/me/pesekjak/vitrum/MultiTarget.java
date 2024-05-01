980bc6190e6539fb400fe836a2f512e8dbe38e03
Pesek <jakub.zahomolkou@gmail.com>
2024-05-01 18:33:55 +0200

    added sources jar, made multi target annotation public

--- vitrum/src/main/java/me/pesekjak/vitrum/MultiTarget.java
+++ vitrum/src/main/java/me/pesekjak/vitrum/MultiTarget.java
@@ -1,13 +1,26 @@
 package me.pesekjak.vitrum;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Annotation used to define multiple targets.
+ * <p>
+ * Use repeatedly {@link Target} instead.
+ */
+@ApiStatus.Internal
 @Retention(RetentionPolicy.RUNTIME)
 @java.lang.annotation.Target(ElementType.METHOD)
-@interface MultiTarget {
+public @interface MultiTarget {
 
+    /**
+     * Targets.
+     *
+     * @return targets
+     */
     Target[] value();
 
 }
